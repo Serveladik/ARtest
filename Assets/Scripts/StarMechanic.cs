5bb2b2278d4a44e086e6560893f0263849d73ab4
Vladyslav <serveladik@gmail.com>
2020-11-26 19:33:37 +0200

    Stars+Pict

---
+++ Assets/Scripts/StarMechanic.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarMechanic : MonoBehaviour
+{
+    [SerializeField] GameObject[] PathList;
+    //public GameObject star;
+    public float speed;
+    float moveTime;
+    Vector3 CurrentPath;
+    int currentPath;
+    void Start()
+    {
+        CheckPath();
+    }
+    void CheckPath()
+    {
+        //star = GetComponent<GameObject>();
+        CurrentPath = PathList[currentPath].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StarMovement();
+    }
+
+    void StarMovement()
+    {
+        
+        if(transform.position!=CurrentPath)
+        {
+            transform.position = Vector3.MoveTowards(transform.position,CurrentPath,speed/1000f);
+        }
+        else
+        {
+            if(currentPath < PathList.Length-1)
+            {
+                currentPath++;
+                CheckPath();
+            }
+            else
+            {
+                currentPath=-1;
+            }
+        }
+        
+    }
+
+
+
+}

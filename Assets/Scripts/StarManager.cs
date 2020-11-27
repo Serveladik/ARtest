ccab221b38d6d51817cf64f4bd87036b25c8e5b4
Vladyslav <serveladik@gmail.com>
2020-11-27 13:21:08 +0200

    aaa

--- Assets/Scripts/StarManager.cs
+++ Assets/Scripts/StarManager.cs
@@ -1,47 +1,64 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class StarManager : StarMechanic
+﻿
+namespace GoogleARCore
 {
-    //public Sprite playSound;
-    [SerializeField] private SpriteRenderer sprite;
-    [SerializeField] private Collider2D collider2d;
-    //[SerializeField] bool touched = false;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using Input = InstantPreviewInput;
     
-    void Start()
+    public class StarManager : StarMechanic
     {
-        sprite = gameObject.GetComponent<SpriteRenderer>();
-        collider2d = gameObject.GetComponent<BoxCollider2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //Touched();
-        //OnMouseDown();
-
-    }
+        //public Sprite playSound;
+        [SerializeField] private SpriteRenderer sprite;
+        [SerializeField] private Collider collider;
+        //[SerializeField] bool touched = false;
 
 
-    public IEnumerator StarSpawning()
-    {
-        yield return new WaitForSecondsRealtime(1);
-        Instantiate(gameObject, startStartPos,Quaternion.identity);
-        yield return new WaitForSecondsRealtime(1);
-    }
+        void Start()
+        {
+            sprite = gameObject.GetComponent<SpriteRenderer>();
+            collider = gameObject.GetComponent<BoxCollider>();
+        }
+    
+        // Update is called once per frame
+        void Update()
+        {
+            //Touched();
+            OnPointerDown();
+    
+        }
+    
+    
+        public IEnumerator StarSpawning()
+        {
+            yield return new WaitForSecondsRealtime(1);
+            Instantiate(gameObject, startStartPos,Quaternion.identity);
+            yield return new WaitForSecondsRealtime(1);
+        }
+    
+        public void Touched()
+        {
+         
+        }
+    
+        void OnPointerDown()
+        {
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+ 
+                var wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                var touchPosition = new Vector2(wp.x, wp.y);
 
-    public void Touched()
-    {
-       if (Input.GetMouseButtonDown(0)) 
-       {
-       }
-    }
-
-    public void OnMouseDown()
-    {
-        
-        Debug.Log("YES");
-        
+                RaycastHit hit;
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.GetTouch(0).position), out hit))
+                {
+                    Debug.Log(hit.transform.name);
+                    if (hit.collider == collider)
+                    {
+                        Destroy(this.gameObject);
+                    }
+                }
+            }
+        }
     }
 }
